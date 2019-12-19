228040e1e69fc9fe9a8ad6a6a0f70eb22664c22d
ederpbj <ederpbj@yahoo.com.br>
2019-12-19 08:25:12 -0300

    git final

---
+++ src/resolvers.js
@@ -0,0 +1,21 @@
+const User = require("./User");
+
+module.exports = {
+ Query: {
+  users: () => User.find(),
+  user: (root, {
+   id
+  }) => User.findById(id)
+ },
+
+ Mutation: {
+  createUser: (root, {
+   name,
+   email
+  }) => User.create({
+   name,
+   email
+  }),
+  
+ }
+};
